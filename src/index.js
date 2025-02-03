c5f9e5d358d753673a5cbadb82bb70a6ccd64724
archwizmgm <46662899+archwizmgm@users.noreply.github.com>
2025-02-03 15:43:30 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,13 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
-
-  // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker.849283.xyz": "https://registry-1.docker.io",
+  "quay.849283.xyz": "https://quay.io",
+  "gcr.849283.xyz": "https://gcr.io",
+  "k8s-gcr.849283.xyz": "https://k8s.gcr.io",
+  "k8s.849283.xyz": "https://registry.k8s.io",
+  "ghcr.849283.xyz": "https://ghcr.io",
+  // "cloudsmith.849283.xyz": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
